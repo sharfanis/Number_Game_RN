061e4b9fa82f11fe173526582bd44d54182a713d
Shabih <Shabih.sharfani@gmail.com>
2020-12-15 13:48:55 -0600

    chore(): Added Image on Game Over Screen
    Logic and importing images from web and local

--- constants/colors.js
+++ constants/colors.js
@@ -3,4 +3,5 @@ export default {
   accent: "yellow",
   acccent2: "black",
   fbColor: "#4267B2",
+  imageGameOver: 'https://www.himalayanwonders.com/siteblog/wp-content/uploads/2014/04/Kangchenjunga.jpg'
 };
--- screens/GameoverScreen.js
+++ screens/GameoverScreen.js
@@ -1,15 +1,29 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, Button, Image } from "react-native";
 import colors from "../constants/colors";
+import BodyText from "../components/BodyText";
+import constants from '../constants/colors';
 
 const GameOverScreen = (props) => {
   return (
     <View style={styles.screen}>
-      <Text>The Game is Over! </Text>
-      <Text>It took you total of {props.rounds} rounds :)</Text>
-      <Text>Number was: {props.userNumber}</Text>
+      <BodyText>The Game is Over! </BodyText>
+      <View style={styles.imageContainer}>
+      <Image style={styles.image} 
+            // source={require("../assets/success.png")} 
+               source = {{uri: constants.imageGameOver}}
+             resizeMode= 'cover'
+             fadeDuration={1000}
+      />
+      </View>
+      <BodyText>It took you total of {props.rounds} rounds :)</BodyText>
+      <BodyText>Number was: {props.userNumber}</BodyText>
       <View style={styles.newGameBtn}>
-      <Button color="white" title="ANOTHER GAME BABE" onPress={props.onRestart}/>
+        <Button
+          color="white"
+          title="ANOTHER GAME BABE"
+          onPress={props.onRestart}
+        />
       </View>
     </View>
   );
@@ -22,17 +36,30 @@ const styles = StyleSheet.create({
     color: "blue",
     fontFamily: "ArialHebrew-Bold",
   },
-  screen:{
-      flex: 1,
-      padding: 10,
-      justifyContent:'center',
-      alignItems:'center'
+  image: {
+    width: '100%',
+    height: '100%',
   },
-  newGameBtn:{
-      marginTop: 20,
-      borderRadius: 8,
-      backgroundColor: colors.fbColor,
-      height: 40,
-      width: 260,
-  }
+  imageContainer: {
+    marginVertical: 30,
+    borderRadius: 200,
+    borderWidth: 3,
+    borderColor: 'black',
+    width: '80%',
+    height: 300,
+    overflow: 'hidden'
+  },
+  screen: {
+    flex: 1,
+    padding: 10,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  newGameBtn: {
+    marginTop: 20,
+    borderRadius: 8,
+    backgroundColor: colors.fbColor,
+    height: 40,
+    width: 260,
+  },
 });

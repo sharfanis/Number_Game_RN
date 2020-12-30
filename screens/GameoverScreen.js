9ecd6b08931b97f5d5f50064e5826f6090749f67
Shabih <Shabih.sharfani@gmail.com>
2020-12-30 16:55:06 -0600

    chore(): GameOVer screen fixes
    added orientation change layout fix logic

--- screens/GameoverScreen.js
+++ screens/GameoverScreen.js
@@ -1,37 +1,77 @@
-import React from "react";
-import { View, Text, StyleSheet, Button, Image , Dimensions ,ScrollView } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  Image,
+  Dimensions,
+  ScrollView,
+} from "react-native";
 import colors from "../constants/colors";
 import BodyText from "../components/BodyText";
-import constants from '../constants/colors';
-import MainButton from '../components/MainButton';
+import constants from "../constants/colors";
+import MainButton from "../components/MainButton";
 
 const GameOverScreen = (props) => {
+  const [availableDeviceWidth, setAvailableDeviceWidth] = useState(
+    Dimensions.get("window").width
+  );
+  const [availableDeviceHeight, setAvailableDeviceHeight] = useState(
+    Dimensions.get("window").height
+  );
+
+  useEffect(() => {
+    const updateLayout = () => {
+      setAvailableDeviceWidth(Dimensions.get("window").width);
+      setAvailableDeviceHeight(Dimensions.get("window").height);
+    };
+
+    Dimensions.addEventListener("change", updateLayout);
+    return () => {
+      Dimensions.removeEventListener("change", updateLayout);
+    };
+  });
+
   return (
     <ScrollView>
-    <View style={styles.screen}>
-      <BodyText>The Game is Over! </BodyText>
-      <View style={styles.imageContainer}>
-      <Image style={styles.image} 
-            // source={require("../assets/success.png")} 
-               source = {{uri: constants.imageGameOver}}
-             resizeMode= 'cover'
-             fadeDuration={1000}
-      />
-      </View>
-      {/* In general the styles are not passed down to the nested child elements , but the only excepttion
+      <View style={styles.screen}>
+        <BodyText>The Game is Over! </BodyText>
+        <View
+          style={{
+            marginVertical: availableDeviceWidth / 20,
+            borderRadius: (availableDeviceWidth * 0.7) / 2,
+            borderWidth: 3,
+            borderColor: "black",
+            width: availableDeviceWidth * 0.7,
+            height: availableDeviceWidth * 0.7,
+            overflow: "hidden",
+          }}
+        >
+          <Image
+            style={{
+              width: availableDeviceWidth > 500 ? "100%" : "100%",
+              height: availableDeviceHeight > 600 ? "100%" : "100%",
+            }}
+            // source={require("../assets/success.png")}
+            source={{ uri: constants.imageGameOver }}
+            resizeMode="cover"
+            fadeDuration={1000}
+          />
+        </View>
+        {/* In general the styles are not passed down to the nested child elements , but the only excepttion
       in RN is for text , the parent Text element( BodyText) will drill down the all the styles to the child text elements too
       so any font color or font size change on the Body text will also reflect on the child elements. */}
-      <BodyText>
-        It took you total of <Text style ={styles.highlight}>{props.rounds}</Text> rounds :)
-        Number was: <Text style ={styles.highlight}>{props.userNumber}</Text>
-      </BodyText>
-      <View style={styles.newGameBtn}>
-        <MainButton
-          onPress={props.onRestart}
-        >ANOTHER GAME BABE ?</MainButton>
+        <BodyText>
+          It took you total of{" "}
+          <Text style={styles.highlight}>{props.rounds}</Text> rounds :) Number
+          was: <Text style={styles.highlight}>{props.userNumber}</Text>
+        </BodyText>
+        <View style={{ marginTop: (availableDeviceHeight * 0.7) / 14}}>
+          <MainButton onPress={props.onRestart}>ANOTHER GAME BABE ?</MainButton>
+        </View>
       </View>
-     </View>
-     </ScrollView>
+    </ScrollView>
   );
 };
 
@@ -42,31 +82,31 @@ const styles = StyleSheet.create({
     color: "blue",
     fontFamily: "open-sans-bold",
   },
-  image: {
-    width: Dimensions.get('window').width > 500 ? '100%' : '100%',
-    height: Dimensions.get('window').height > 600 ? '100%' : '100%',
-  },
-  imageContainer: {
-    marginVertical: Dimensions.get('window').height / 20,
-    borderRadius: Dimensions.get('window').width * 0.7 / 2,
-    borderWidth: 3,
-    borderColor: 'black',
-    width: Dimensions.get('window').height > 500 ? 300 : 200,
-    height: Dimensions.get('window').height > 500 ? 300 : 200,
-    overflow: 'hidden'
-  },
-   highlight:{
+  // image: {
+  //   width: availableDeviceWidth > 500 ? "100%" : "100%",
+  //   height: availableDeviceHeight > 600 ? "100%" : "100%",
+  // },
+  // imageContainer: {
+  //   marginVertical: availableDeviceWidth / 20,
+  //   borderRadius: (availableDeviceWidth * 0.7) / 2,
+  //   borderWidth: 3,
+  //   borderColor: "black",
+  //   width: availableDeviceWidth * 0.7,
+  //   height: availableDeviceWidth * 0.7,
+  //   overflow: "hidden",
+  // },
+  highlight: {
     color: colors.primary,
-    textAlign: 'center'
+    textAlign: "center",
   },
-    screen: {
+  screen: {
     flex: 1,
     padding: 10,
     justifyContent: "center",
     alignItems: "center",
   },
   newGameBtn: {
-   marginTop : Dimensions.get('window').height * 0.7 / 14 ,
+    // marginTop: (availableDeviceHeight * 0.7) / 14,
     // borderRadius: 8,
     // backgroundColor: colors.fbColor,
     // height: 40,

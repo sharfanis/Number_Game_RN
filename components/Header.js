4734a8934e6c752e3692f2bf7f6fb340d889039c
Shabih <Shabih.sharfani@gmail.com>
2020-12-30 17:31:41 -0600

    chore(): Ripple effect on button basedd on platform
    identfying platform and changing the ripple effect on buttons

--- components/Header.js
+++ components/Header.js
@@ -1,36 +1,61 @@
-import React, { Component } from 'react'
-import { View , Text , StyleSheet , Platform } from 'react-native'
-import colors from '../constants/colors';
-import BodyText from './BodyText';
+import React, { Component } from "react";
+import { View, Text, StyleSheet, Platform } from "react-native";
+import colors from "../constants/colors";
+import BodyText from "./BodyText";
 
-const Header = props => {
- return(
-     <View style={styles.header}>
-         <BodyText style={styles.headerTitle}>{props.title}</BodyText>
-     </View>
- )
-
+const Header = (props) => {
+  return (
+    <View
+      style={{
+        ...styles.headerBase,
+        ...Platform.select({
+          ios: styles.headerIOS,
+          android: styles.headerAndroid,
+        }),
+      }}
+    >
+      <BodyText
+        style={{
+          ...styles.headerTitle,
+          ...Platform.select({
+            ios: styles.headerTitleIOS,
+            android: styles.headerTitleAndroid,
+          }),
+        }}
+      >
+        {props.title}
+      </BodyText>
+    </View>
+  );
 };
 
-
 const styles = StyleSheet.create({
-    header: {
-    borderBottomColor: Platform.OS === 'ios' ?  '#ccc' : 'transparent' ,
-    borderBottomWidth: Platform.OS === 'ios' ?  1 : 0 ,
-    width: '100%',
+  headerBase: {
+    width: "100%",
     height: 90,
     paddingTop: 36,
-    backgroundColor: Platform.OS === 'android' ? colors.fbColor : colors.acccent2,
-    alignItems: 'center',
-    justifyContent: 'center'
-    },
-    headerTitle:{
-        color: Platform.OS === 'android' ? colors.accent : colors.fbColor,
-        fontSize: 18
-
-    },
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  headerIOS: {
+    backgroundColor: colors.acccent2,
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1,
+  },
+  headerAndroid: {
+    backgroundColor: colors.fbColor,
+    borderBottomColor: "transparent",
+    borderBottomWidth: 0,
+  },
+  headerTitleIOS: {
+    color: colors.fbColor,
+  },
+  headerTitleAndroid: {
+    color: colors.accent,
+  },
+  headerTitle: {
+    fontSize: 18,
+  },
 });
 
 export default Header;
-
-

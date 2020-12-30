67617a24cc164ea7ff3ce23f71dbb2442423de05
Shabih <Shabih.sharfani@gmail.com>
2020-12-30 17:53:29 -0600

    chore(): addded safeareaview around for Iphone Bezels distorting our UI

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View ,SafeAreaView } from "react-native";
 import Header from "./components/Header";
 import GameScreen from "./screens/GameScreen";
 import StartGameScreen from "./screens/StartGameScreen";
@@ -55,10 +55,10 @@ export default function App() {
   }
 
   return (
-    <View style={styles.screen}>
+    <SafeAreaView style={styles.screen}>
       <Header title="Guess A Number !" />
       {content}
-    </View>
+    </SafeAreaView>
   );
 }
 
